192117307c0971d69617ad2739a6c3ee4590a3ac
krysztof <cbaunach@reach.sh>
2022-06-08 13:18:14 -0500

    fix error messages

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ export const ASSURANCE_MSG = `Your funds were not moved.`;
 export const MIN_BALANCE_MSG = `Transaction would drop account below minimum required balance.`;
 export const POPUP_BLOCKED_MSG = `Your browser is blocking popups; please disable this blocker before retrying.`;
 export const TRANSACTION_CANCELLED_MSG = `Transaction Cancelled.`;
+export const TRANSACTION_DIDNT_LOAD = `Sorry, there was some connection issues and the transaction window didn't load; please try again`;
 export const POOL_CREATION_ERR = `Pool creation failed.`;
 // Fees
 export const FLOAT = 0.0001;

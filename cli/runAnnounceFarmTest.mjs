0a37db2dd0d8479f87b4395a3f4f688313189fdb
MrJackdaw <MrJackdaw@users.noreply.github.com>
2022-11-21 03:44:59 -0800

    * cleanup cli run-farm-announcer script

--- cli/runAnnounceFarmTest.mjs
+++ cli/runAnnounceFarmTest.mjs
@@ -5,11 +5,16 @@ import {
   fetchFarmAndTokens,
   fetchLiquidityPool,
   fetchStakingPool,
+  formatCurrency,
   getNetworkProvider,
+  isNetworkToken,
   isPartnerFarm,
+  parseAddress,
+  parseCurrency,
   subscribeToFarmStream
 } from "@reach-sh/humble-sdk";
 import { yesno } from "@reach-sh/stdlib/ask.mjs";
+import { estimateRewardsPerBlock } from "../lib/api/FarmStream.Announce.js";
 import {
   exitWithMsgs,
   Blue,
@@ -17,7 +22,8 @@ import {
   Yellow,
   iout,
   Green,
-  answerOrDie
+  answerOrDie,
+  rerunOrExit
 } from "./utils.mjs";
 
 let exitTimeout;
@@ -29,7 +35,7 @@ const pools = new Set();
   Announced 08/18
 =====================
 farm          pool
-123132710     122708890
+123132710 122708890
  */
 
 /** Announce a single farm */
@@ -43,13 +49,10 @@ export async function runAnnounceFarmTest(acc) {
     formatResult: false,
     includeTokens: true
   });
+  if (!farmResult.succeeded) return exitWithMsgs(`Farm id ${farmId} not found`);
 
-  if (!farmResult.succeeded) {
-    return exitWithMsgs(`Farm id ${farmId} not found`);
-  }
-
-  const { farmView: farm, stakeToken, rewardToken } = farmResult.data;
-  const isPartner = isPartnerFarm({ farmView: farm });
+  const { farmView, stakeToken, rewardToken } = farmResult.data;
+  const isPartner = isPartnerFarm({ farmView });
   const label = isPartner ? `PARTNER FARM` : `FARM`;
   Green(`Fetched ${label} id ${farmId}`);
   Blue(JSON.stringify(farmResult.data, null, 2));
@@ -70,30 +73,55 @@ export async function runAnnounceFarmTest(acc) {
 
   if (!poolResult.succeeded) return exitWithMsgs(`Pool id ${poolId} not found`);
   const { data: poolData } = poolResult;
+  const { pool, tokens } = poolData;
+  const [tokA, tokB] = tokens;
   const { contract: farmContract } = farmResult;
   const stdlib = createReachAPI();
-  if (poolData.pool.poolTokenId.toString() !== stakeToken.id.toString()) {
+  if (pool.poolTokenId.toString() !== stakeToken.id.toString()) {
     return exitWithMsgs("Pool Liquidity Token does not match Farm Stake Token");
   }
 
+  const big = stdlib.bigNumberify;
+  const div = stdlib.div;
   const stakeTokenRaw = await acc.tokenMetadata(stakeToken.id);
+  const duration = farmView.opts.end - farmView.opts.start;
+  const { totalRewards: tr } = farmView;
+  const [nr, nnr] = [
+    tr.network,
+    tr.rewardToken
+    // Number(formatCurrency(tr.network)),
+    // Number(formatCurrency(tr.rewardToken, stakeToken.decimals))
+  ];
+  const rpb = estimateRewardsPerBlock([nr, nnr], duration);
+  Red("=============");
+  iout("total rewards", [nr, nnr]);
+  iout("duration (blocks)", duration);
+  iout("estimate rewards per block", rpb);
+  iout("parsed rewards", [
+    formatCurrency(parseCurrency(rpb[0])),
+    formatCurrency(parseCurrency(rpb[1], tokB.decimals), tokB.decimals)
+  ]);
+  Red("=============");
+
+  const Ceiling = (s) => Math.ceil(Number(s));
+  const AId = isNetworkToken(tokA.id) ? null : parseAddress(tokA.id);
   const staticFarmData = {
-    ctcInfo: stdlib.bigNumberify(farmId),
-    endBlock: farm.opts.end,
-    pairTokenAId: asMaybe(null),
-    pairTokenASymbol: poolData.tokens[0].symbol,
-    pairTokenBId: stdlib.bigNumberify(poolData.tokens[1].id),
-    pairTokenBSymbol: poolData.tokens[1].symbol,
-    rewardsPerBlock: farm.opts.rewardsPerBlock,
-    rewardTokenDecimals: stdlib.bigNumberify(rewardToken.decimals),
-    rewardTokenId: farm.opts.rewardToken1,
+    ctcInfo: parseAddress(farmId),
+    endBlock: farmView.opts.end,
+    pairTokenAId: asMaybe(AId),
+    pairTokenASymbol: tokA.symbol,
+    pairTokenBId: parseAddress(tokB.id),
+    pairTokenBSymbol: tokB.symbol,
+    rewardsPerBlock: rpb.map(Ceiling),
+    rewardTokenDecimals: rewardToken.decimals,
+    rewardTokenId: parseAddress(rewardToken.id),
     rewardTokenSymbol: rewardToken.symbol,
-    stakedTokenDecimals: stdlib.bigNumberify(stakeToken.decimals),
-    stakedTokenId: farm.opts.stakeToken,
-    stakedTokenPoolId: stdlib.bigNumberify(poolId),
+    stakedTokenDecimals: stakeToken.decimals,
+    stakedTokenId: parseAddress(stakeToken.id),
+    stakedTokenPoolId: parseAddress(poolId),
     stakedTokenSymbol: stakeToken.symbol,
-    stakedTokenTotalSupply: stakeTokenRaw.supply,
-    startBlock: farm.opts.start
+    stakedTokenTotalSupply: big(stakeToken.supply),
+    startBlock: farmView.opts.start
   };
 
   iout(`Ready to announce ${label} "${farmId}"`, staticFarmData);
@@ -113,7 +141,8 @@ export async function runAnnounceFarmTest(acc) {
   console.log();
   console.log();
 
-  return (await answerOrDie("Announce another farm? (y/n)")) === "y"
-    ? runAnnounceFarmTest(acc)
-    : exitWithMsgs("Done");
+  return rerunOrExit({
+    do: () => runAnnounceFarmTest(acc),
+    prompt: "Announce another farm? (y/n)"
+  });
 }
